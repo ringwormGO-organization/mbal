583579dffe3e5e899625da9a6cc694bc0962fa57
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2021-12-14 16:33:54 +0100

    mbal editor - code

---
+++ mbal editor/mbal editor/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ElementHost1 = New System.Windows.Forms.Integration.ElementHost()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.PythonInstall = New System.Windows.Forms.TextBox()
+        Me.mbal = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.OpenFile = New System.Windows.Forms.Button()
+        Me.SaveFile = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(991, 635)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(139, 31)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Info"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ElementHost1
+        '
+        Me.ElementHost1.Location = New System.Drawing.Point(719, 538)
+        Me.ElementHost1.Name = "ElementHost1"
+        Me.ElementHost1.Size = New System.Drawing.Size(190, 72)
+        Me.ElementHost1.TabIndex = 3
+        Me.ElementHost1.Text = "ElementHost1"
+        Me.ElementHost1.Child = Nothing
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.RichTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.RichTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(13, 69)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(1117, 560)
+        Me.RichTextBox1.TabIndex = 4
+        Me.RichTextBox1.Text = ""
+        '
+        'PythonInstall
+        '
+        Me.PythonInstall.Location = New System.Drawing.Point(13, 13)
+        Me.PythonInstall.Name = "PythonInstall"
+        Me.PythonInstall.Size = New System.Drawing.Size(269, 22)
+        Me.PythonInstall.TabIndex = 5
+        Me.PythonInstall.Text = "Python Executable"
+        Me.PythonInstall.Visible = False
+        '
+        'mbal
+        '
+        Me.mbal.Location = New System.Drawing.Point(12, 41)
+        Me.mbal.Name = "mbal"
+        Me.mbal.Size = New System.Drawing.Size(269, 22)
+        Me.mbal.TabIndex = 5
+        Me.mbal.Text = "shell.py"
+        Me.mbal.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.ForeColor = System.Drawing.Color.Lime
+        Me.Button1.Location = New System.Drawing.Point(301, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(157, 52)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(848, 13)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(282, 50)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Visible = False
+        '
+        'OpenFile
+        '
+        Me.OpenFile.Location = New System.Drawing.Point(13, 635)
+        Me.OpenFile.Name = "OpenFile"
+        Me.OpenFile.Size = New System.Drawing.Size(130, 31)
+        Me.OpenFile.TabIndex = 8
+        Me.OpenFile.Text = "Open File"
+        Me.OpenFile.UseVisualStyleBackColor = True
+        '
+        'SaveFile
+        '
+        Me.SaveFile.Location = New System.Drawing.Point(149, 635)
+        Me.SaveFile.Name = "SaveFile"
+        Me.SaveFile.Size = New System.Drawing.Size(130, 31)
+        Me.SaveFile.TabIndex = 8
+        Me.SaveFile.Text = "Save File"
+        Me.SaveFile.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label1.Location = New System.Drawing.Point(464, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(373, 39)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Welcome to mbal editor"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1142, 678)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.SaveFile)
+        Me.Controls.Add(Me.OpenFile)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.mbal)
+        Me.Controls.Add(Me.PythonInstall)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.ElementHost1)
+        Me.Controls.Add(Me.Button2)
+        Me.Name = "Form1"
+        Me.Text = "mbal editor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ElementHost1 As Integration.ElementHost
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents PythonInstall As TextBox
+    Friend WithEvents mbal As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents OpenFile As Button
+    Friend WithEvents SaveFile As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents Label1 As Label
+End Class
---
+++ mbal editor/mbal editor/Form1.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Public Value As String
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim Info = MessageBox.Show("Editor for mbal" & vbNewLine & "Create new *.mbal file, save it, run mbal by hand", "Info [mbal editor]")
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+        'If RichTextBox1.Text.Contains = "PRINT" Then
+        'RichTextBox1.ForeColor = Color.Purple
+        'End If
+
+        'Dim tokens As String = "(regex|dim|object|new|string|public|integer|end|auto|double|int|struct|break|else|long|switch|case|enum|register|typedef|char|extern|return|union|const|float|short|unsigned|continue|for|signed|void|default|goto|sizeof|volatile|do|if|static|while|1|2|3|4|5|6|7|8|9|my)"
+        Dim myToken As String = "(NULL|FALSE|TRUE|MATH_PI|PRINT|PRINT_RET|INPUT|INPUT_INT|CLEAR|CLS|IS_NUM|IS_STR|IS_LIST|IS_FUN|APPEND|POP|EXTEND|LEN|RUN|VAR|IF|ELSE|THEN|FOR|END|FUN|RETURN)"
+        Dim rex As New Regex(myToken)
+        Dim mc As MatchCollection = rex.Matches(RichTextBox1.Text)
+        Dim StartCursorPosition As Integer = RichTextBox1.SelectionStart
+        For Each m As Match In mc
+            Dim startIndex As Integer = m.Index
+            Dim StopIndex As Integer = m.Length
+            RichTextBox1.[Select](startIndex, StopIndex)
+            RichTextBox1.SelectionColor = Color.Blue
+            RichTextBox1.SelectionStart = StartCursorPosition
+
+            RichTextBox1.SelectionColor = Color.Black
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Dim proc As Process = New Process
+
+        'proc.StartInfo.FileName = PythonInstall.Text
+        'proc.StartInfo.Arguments = mbal.Text
+
+        'proc.StartInfo.UseShellExecute = False
+        'proc.StartInfo.WindowStyle = ProcessWindowStyle.Normal
+        'proc.StartInfo.CreateNoWindow = False
+        'proc.StartInfo.RedirectStandardOutput = True
+
+        'proc.Start()
+
+        'AddHandler proc.OutputDataReceived, AddressOf process_Output_DataReceived
+
+        'proc.BeginOutputReadLine()
+        ''proc.WaitForExit()
+
+        'TextBox1.Text = Value
+    End Sub
+
+    Private Sub process_Output_DataReceived(sender As Object, e As DataReceivedEventArgs)
+        On Error Resume Next
+        If e.Data = "" Then
+
+        Else
+            Value = e.Data
+        End If
+    End Sub
+
+    Private Sub OpenFile_Click(sender As Object, e As EventArgs) Handles OpenFile.Click
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            RichTextBox1.Text = My.Computer.FileSystem.ReadAllText(OpenFileDialog1.FileName)
+        End If
+    End Sub
+
+    Private Sub SaveFile_Click(sender As Object, e As EventArgs) Handles SaveFile.Click
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, RichTextBox1.Text, False)
+        End If
+    End Sub
+End Class
